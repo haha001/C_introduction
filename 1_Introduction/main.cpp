551c87c1d23eb00733cf7ff8adca6be8a61170a5
Simon Vu <simon_vu1@hotmail.com>
2016-08-30 19:58:17 +0200

    Added all the source code

--- 1_Introduction/main.cpp
+++ 1_Introduction/main.cpp
@@ -1,4 +1,6 @@
-//Include standard I/O library
+/*Include standard I/O library
+Lines starting with # is for the C preprocesssor
+*/
 #include <stdio.h>
 
 //main function as starting point of C programs
