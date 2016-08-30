dda3ecfe89fcb29b568f31b60806ad2ea2717015
Simon Vu <simon_vu1@hotmail.com>
2016-08-30 20:07:37 +0200

    Added integer read and print

--- 1_Introduction/main.cpp
+++ 1_Introduction/main.cpp
@@ -6,5 +6,18 @@ Lines starting with # is for the C preprocesssor
 //main function as starting point of C programs
 int main()
 {
+	int x, y;
 	printf("mit navn er simon\n");
+
+	printf("Enter the first integer"); //Prompt
+	scanf("%d", &x); //read first integer
+
+	printf("Enter the second integer"); //Promt
+	scanf("%d", &y); //Read second integer
+
+	int sum; //Create sum variable
+	sum = x + y; //add x and y to sum
+
+	printf("Sum is %d\n", sum);
+
 } //End of function
