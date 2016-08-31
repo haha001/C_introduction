b67b330a3ef9d01363286e5fd1d62aada2fedfd7
Simon Vu <simon_vu1@hotmail.com>
2016-08-31 08:47:41 +0200

    Just playing around with spaces/tabs

--- 1_Introduction/main.cpp
+++ 1_Introduction/main.cpp
@@ -8,16 +8,18 @@ int main(void)
 {
 	int x, y;
 	printf("mit navn er simon\n");
-
+	
 	printf("Enter the first integer"); //Prompt
 	scanf("%d", &x); //read first integer
-
+	
 	printf("Enter the second integer"); //Promt
 	scanf("%d", &y); //Read second integer
-
+	
 	int sum; //Create sum variable
 	sum = x + y; //add x and y to sum
 
 	printf("Sum is %d\n", sum);
+
+
 
 } //End of function
